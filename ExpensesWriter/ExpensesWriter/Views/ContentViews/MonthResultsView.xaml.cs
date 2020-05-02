d0a526638a6976da439ee3f2f32a2dd892478948
sergiy.velychko <v-svelychko@bio-optronics.com>
2020-05-02 21:55:08 +0300

    One month results view for all month results pages

---
+++ ExpensesWriter/ExpensesWriter/Views/ContentViews/MonthResultsView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExpensesWriter.Views.ContentViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonthResultsView : ContentView
+    {
+        public MonthResultsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
